a6953d66811afecc08f236e3bea34a1310d80333
sojwalwherrelz <104896718+sojwalwherrelz@users.noreply.github.com>
2022-05-09 14:40:25 +0530

    Update PlayerSearch.js

--- PlayerSearch.js
+++ PlayerSearch.js
@@ -2,7 +2,7 @@ var axios  =  require('axios');
 const port = 8080;
 const Name = "Olive Smith"
 const playerName = Name.replace(" ", "%20");
-const apikey = "da2589ff-76cf-4b11-8654-080868ac896b";
+const apikey = "%APIKEY%";
 axios.get(`https://api.cricapi.com/v1/players?apikey=${apikey}&offset=0&search=${playerName}`)
 .then((result)=>{    
     var data = result.data.data;
