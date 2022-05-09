c98672ac7a0e6c4810719b57bbf65a9b5115cc38
sojwalwherrelz <104896718+sojwalwherrelz@users.noreply.github.com>
2022-05-09 14:41:10 +0530

    Update countries.js

--- countries.js
+++ countries.js
@@ -1,6 +1,6 @@
 var axios  =  require('axios');
 const port = 8080;
-const apikey = "da2589ff-76cf-4b11-8654-080868ac896b";
+const apikey = "%APIKEY%";
 axios.get(`https://api.cricapi.com/v1/countries?apikey=${apikey}&offset=0`)
 .then((result)=>{
     console.log(result.data.data);
