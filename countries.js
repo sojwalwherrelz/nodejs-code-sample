41e4879428b779a52c4cd300766cde91c3e8cfb5
sojwalwherrelz <104896718+sojwalwherrelz@users.noreply.github.com>
2022-05-10 12:05:25 +0530

    Update countries.js
    remove console .log

--- countries.js
+++ countries.js
@@ -3,7 +3,6 @@ const port = 8080;
 const apikey = "%APIKEY%";
 axios.get(`https://api.cricapi.com/v1/countries?apikey=${apikey}&offset=0`)
 .then((result)=>{
-    console.log(result.data.data);
     var data = result.data.data;
     data.forEach(element => {
         console.log("country Name : "+element.name +' - country Flag Src :'+ element.genericFlag+"\n");
